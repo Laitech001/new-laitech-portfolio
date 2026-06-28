f26d79e6295de6137ed5907dc927fd00c18c76fe
Abdulganiy Ibrahim <abdulganiyibrahim095@gmail.com>
2026-06-29 00:51:57 +0100

    FIXED: fixed build error

--- components/layout/index.ts
+++ components/layout/index.ts
@@ -1,4 +1,3 @@
 export { default as AmbientBackground } from './AmbientBackground';
 export { default as Footer } from './Footer';
 export { default as Header } from './Header';
-export { default as MobileMenu } from './MobileMenu'
